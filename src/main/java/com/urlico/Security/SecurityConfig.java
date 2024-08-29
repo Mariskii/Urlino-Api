9599886db782cd1455ec0c7a664494a277d7b1f5
Vicent Paya <vicentpayaginer@gmail.com>
2024-08-29 22:31:55 +0200

    fix: allowed origis http => https

--- src/main/java/com/urlico/Security/SecurityConfig.java
+++ src/main/java/com/urlico/Security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://urlino.icu"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://urlino.icu"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
