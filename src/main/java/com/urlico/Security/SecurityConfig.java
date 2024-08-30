ecf3513a7ef99a04d711eca00a33280e4310d891
Vicent Paya <vicentpayaginer@gmail.com>
2024-08-30 17:04:27 +0200

    fix: set allowed origin without /home

--- src/main/java/com/urlico/Security/SecurityConfig.java
+++ src/main/java/com/urlico/Security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://urlino.icu", "https://urlino.netlify.app/home"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200",  "https://urlino.netlify.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
